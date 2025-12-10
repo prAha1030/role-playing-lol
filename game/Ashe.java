713ba0e3b51077b78e133760d6992bc83dbb7efb
praha1030 <hunmin111@gmail.com>
2025-12-10 15:00:34 +0900

    garen&ashe basic setting

--- game/Ashe.java
+++ game/Ashe.java
@@ -1,4 +1,17 @@
 package game;
 
 public class Ashe {
+    String name;
+    int level;
+    int hp;
+    int attackDamage;
+    int defence;
+
+    public Ashe(String name, int level, int hp, int attackDamage, int defence) {
+        this.name = name;
+        this.level = 1;
+        this.hp = 450;
+        this.attackDamage = 60;
+        this.defence = 35;
+    }
 }
--- game/Garen.java
+++ game/Garen.java
@@ -1,4 +1,17 @@
 package game;
 
 public class Garen {
+    String name;
+    int level;
+    int hp;
+    int attackDamage;
+    int defence;
+
+    public Garen(String name, int level, int hp, int attackDamage, int defence) {
+        this.name = name;
+        this.level = 1;
+        this.hp = 600;
+        this.attackDamage = 50;
+        this.defence = 40;
+    }
 }
