8506d56e8a2437da8571bc38de19fceb63e2bd6f
곽현민 <hunmin111@gmail.com>
2025-12-13 16:43:51 +0900

    level up& basic stat

---
+++ game/GameConstants.java
@@ -0,0 +1,11 @@
+package game;
+
+public class GameConstants {
+    public static final int MAX_LEVEL = 18;
+    public static final int BASIC_HP = 400;
+    public static final int BASIC_ATTACK_DAMAGE = 40;
+    public static final int BASIC_DEFENCE = 20;
+
+    // 인스턴스화 금지
+    private GameConstants() {}
+}
